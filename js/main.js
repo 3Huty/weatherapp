1505956c4cea56826cc8af1e3c6d6684e7d9feba
3Huty <sawczuk.a@gmail.com>
2018-06-29 10:36:25 +0200

    add main.js

--- js/main.js
+++ js/main.js
@@ -1,38 +1,43 @@
-if("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        loadWeather(position.coords.latitude + ',' + position.coords.longitude);
-    });
-} else {
-    loadWeather("Kolkata, IN", "");
+function getWeather(){
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            loadWeather(position.coords.latitude + ',' + position.coords.longitude, "");
+        });
+    } else {
+        loadWeather("Kolkata, IN", "");  
+    }
 }
-
 $(document).ready(function() {
-    setInterval(loadWeather, 10000);
+    setInterval(getWeather, 10000);  
 });
-                  
-function loadWeather(location, woeid) {
+
+function loadWeather(location, woeid) {  
     $.simpleWeather({
         location: location,
         woeid: woeid,
-        unit: 'c',
+        unit: "c",
         success: function(weather) {
             city = weather.city;
-            temp = weather.temp+'&deg;';
-            wcode = '<img class="weathericon" src="images/weathericons/' + weather.code + '.svg">';
-            wind = '<p>' + weather.wind.speed + '</p><p>' + weather.units.speed + '</p>';
-            humidity = weather.humidity + ' %';
+            temp = weather.temp + "&deg";
+            wcode = '<img class="weathericon" src="images/weathericons/' + weather.code + '.svg" />';
+            wind = "<p>" + weather.wind.speed + "</p><p>" + weather.units.speed + "</p>";
+            humidity = weather.humidity + " %";
             
             $(".location").text(city);
             $(".temperature").html(temp);
             $(".climate_bg").html(wcode);
-            $(".humidity").text(humidity);
             $(".windspeed").html(wind);
+            $(".humidity").text(humidity);
         },
         
         error: function(error) {
-            $(".error").html('<p>' + error + '</p>');
+            console.log("Error: " + error);
         }
     });
 }
-        
-  
+
+$(document).ready(function() {
+    getWeather();
+    window.setInterval(getWeather, 10000);
+});
+
